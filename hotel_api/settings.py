fa8d9cc2d4b4427df0b2ab5079862653f9d54e3c
bikram <bikram@gmail.com>
2023-05-30 02:26:49 +0545

    remote change

--- hotel_api/settings.py
+++ hotel_api/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+qu@v2j%x14*b_fcin4pn)gd4*tb1h5a23&eq=on2n0)k=l0%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','Bikrm10.pythonanywhere.com']
 
 
 # Application definition
@@ -104,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
